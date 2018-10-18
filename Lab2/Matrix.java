b316db3f3b8035aef3a5172fc7e4acbe6026b73a
Adrian Janakiewicz <ajanakie@mops.uci.agh.edu.pl>
2018-10-18 13:34:05 +0200

    Init + first

---
+++ Lab2/Matrix.java
@@ -0,0 +1,15 @@
+package Lab2;
+
+public class Matrix {
+    double[] data;
+    int rows;
+    int cols;
+
+    Matrix(int rows, int cols){
+        this.rows = rows;
+        this.cols = cols;
+        data = new double[rows*cols];
+    }
+    //...
+
+}
