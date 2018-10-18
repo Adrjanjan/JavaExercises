c18630828aa1dc38287a7f92867edbcfc425be7a
Adrian Janakiewicz <adrjanjan@gmail.com>
2018-10-18 23:05:40 +0200

    Lab2 finished.

---
+++ src/Lab2/Matrix.java
@@ -0,0 +1,163 @@
+package Lab2;
+
+public class Matrix {
+    private double[]data;
+    private int rows;
+    private int cols;
+
+    Matrix(int rows, int cols) throws ArrayIndexOutOfBoundsException{
+        if (rows <= 0 || cols <= 0)
+            throw new ArrayIndexOutOfBoundsException(String.format("Rows or cols is not positive: \nRows: %d\nCols: %d", rows,cols));
+        this.rows = rows;
+        this.cols = cols;
+        data = new double[rows * cols];
+    }
+
+    Matrix(double[][] d){
+        rows = d.length;
+        int max =0;
+        for(int i =0; i< rows; ++i){
+            max = (d[i].length > max)? d[i].length: max;
+        }
+        cols = max;
+        data = new double[rows*cols];
+        for(int i =0; i< d.length; ++i){
+            System.arraycopy(d[i], 0, data, i * max, d[i].length);
+        }
+    }
+
+    double[][] asArray(){
+        double[][] twoD = new double[rows][cols];
+        for(int i=0; i<rows; ++i){
+            if (cols >= 0) System.arraycopy(data, i * rows, twoD[i], 0, cols);
+        }
+
+        return twoD;
+    }
+
+    double get(int r,int c) throws ArrayIndexOutOfBoundsException{
+        if (r >= rows || c >= cols) throw new ArrayIndexOutOfBoundsException("To big rows or cols:" + "\nRows: " + rows + "\nCols: " + cols);
+        return data[r * rows + c];
+    }
+    void set (int r,int c, double value)throws ArrayIndexOutOfBoundsException{
+        if (r >= rows || c >= cols) throw new ArrayIndexOutOfBoundsException("To big rows or cols:" + "\nRows: " + rows + "\nCols: " + cols);
+        data[r * rows + c] = value;
+    }
+
+    public String toString(){
+        StringBuilder buf = new StringBuilder();
+        buf.append("[");
+        for(int i=0;i<rows;i++){
+            buf.append("[");
+            for(int j=0; j<cols; ++j){
+                buf.append(data[i * rows + j]).append(", ");
+            }
+            buf.replace(buf.length()-1, buf.length()-1, "],\n");
+        }
+        buf.replace(buf.length()-3, buf.length(), "]\n");
+        //buf.replace(buf.length()-1, buf.length()-1, "]]");
+        return buf.toString();
+    }
+
+    void reshape(int newRows,int newCols) throws Exception {
+        if (rows * cols != newRows * newCols)
+            throw new Exception(String.format("%d x %d matrix can't be reshaped to %d x %d", rows, cols, newRows, newCols));
+
+        rows = newRows;
+        cols = newCols;
+    }
+
+    int[] shape(){
+        return new int[]{rows, cols};
+    }
+
+    Matrix add(Matrix m) {
+        if (rows != m.rows || cols != m.cols)
+            throw new ArrayIndexOutOfBoundsException("Matrices are not in the same shape");
+
+        Matrix temp = new Matrix(rows, cols);
+        for(int i=0; i< data.length; ++i){
+            temp.data[i] = data[i] + m.data[i];
+        }
+        return temp;
+    }
+
+    Matrix sub(Matrix m){
+        return this.add(m.mul(-1));
+    }
+    Matrix mul(Matrix m){
+        if (rows != m.rows || cols != m.cols)
+            throw new ArrayIndexOutOfBoundsException("Matrices are not in the same shape");
+
+        Matrix temp = new Matrix(rows, cols);
+        for(int i=0; i< data.length; ++i){
+            temp.data[i] = data[i] * m.data[i];
+        }
+        return temp;
+    }
+    Matrix div(Matrix m) {
+        if (rows != m.rows || cols != m.cols)
+            throw new ArrayIndexOutOfBoundsException("Matrices are not in the same shape");
+
+        Matrix temp = new Matrix(rows, cols);
+        for(int i=0; i< data.length; ++i){
+            temp.data[i] = data[i] / m.data[i];
+        }
+        return temp;
+    }
+
+    Matrix add(double w){
+        Matrix temp = new Matrix(rows, cols);
+        for(int i=0; i< data.length; ++i){
+            temp.data[i] = data[i] + w;
+        }
+        return temp;
+    }
+    Matrix sub(double w){
+        Matrix temp = new Matrix(rows, cols);
+        for(int i=0; i< data.length; ++i){
+            temp.data[i] = data[i] - w;
+        }
+        return temp;
+    }
+    Matrix mul(double w){Matrix temp = new Matrix(rows, cols);
+        for(int i=0; i< data.length; ++i){
+            temp.data[i] = data[i] * w;
+        }
+        return temp;}
+    Matrix div(double w){Matrix temp = new Matrix(rows, cols);
+        for(int i=0; i< data.length; ++i){
+            temp.data[i] = data[i] / w;
+        }
+        return temp;}
+
+    Matrix dot(Matrix m) throws ArrayIndexOutOfBoundsException{
+        if(cols != m.rows) throw new ArrayIndexOutOfBoundsException("Not compatible dimensions.");
+
+        double sum;
+        Matrix tmp = new Matrix(rows, m.cols);
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < m.cols; ++j) {
+                sum = 0;
+                for (int k = 0; k < cols; ++k) {
+                    sum += get(i, k) * m.get(k, j);
+                }
+                tmp.set(i, j, sum);
+            }
+        }
+        return tmp;
+    }
+
+    double frobenius(){
+        double temp = 0;
+
+        for(int i=0; i< rows; ++i){
+            for(int j=0; j< cols; ++j){
+                temp += get(i,j) * get(i, j);
+            }
+        }
+        return Math.sqrt(temp);
+    }
+
+}
--- src/Lab2/MatrixTest.java
+++ src/Lab2/MatrixTest.java
@@ -1,4 +1,40 @@
 package Lab2;
 
+import java.util.Arrays;
+
 public class MatrixTest {
+
+    public static void main(String[] args) throws Exception {
+        //testMatrix();
+        //testAdd();
+
+        Matrix m = new Matrix(new double[][]{{1,2,3,4},{5,6},{7,8},{9}});
+
+        System.out.println(m);
+        int a=2, b=8;
+        m.reshape(a, b);
+
+        System.out.println(Arrays.deepToString(m.asArray()));
+
+        System.out.println("Shape " + Arrays.toString(m.shape()));
+
+        System.out.println(m.add(m));
+        System.out.println(m.sub(m));
+        System.out.println(m.mul(m));
+        System.out.println(m.div(m));
+        double d = 1.5;
+        System.out.println(m.add(d));
+        System.out.println(m.sub(d));
+        System.out.println(m.mul(d));
+        System.out.println(m.div(d));
+
+        m.reshape(4,4);
+        System.out.println(m.dot(m));
+
+        System.out.println(m.add(m));
+        System.out.println(m.sub(m));
+        System.out.println(m.mul(m));
+        System.out.println(m.frobenius());
+
+    }
 }
